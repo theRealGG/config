b2936603516944195de474a875afa472a9db8557
Hisham Görner <43104578+theRealGG@users.noreply.github.com>
2023-05-10 16:43:50 +0200

    Add files via upload

---
+++ nvim/lua/editor_actions.lua
@@ -0,0 +1,71 @@
+local status, null_ls = pcall(require, "null-ls")
+if not status then
+  return
+end
+
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method("textDocument/rangeFormatting") then
+      vim.keymap.set("x", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.google_java_format,
+    null_ls.builtins.diagnostics.stylelint.with({
+      filetypes = {
+        "css",
+        "scss",
+      },
+    }),
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = "[eslint] #{m}\n(#{c})",
+    }),
+    null_ls.builtins.diagnostics.fish,
+  },
+})
+
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = "prettier", -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+})
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,64 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd([[packadd packer.nvim]])
+
+return require("packer").startup(function(use)
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
+  use("Mofiqul/dracula.nvim")
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "nvim-tree/nvim-web-devicons", opt = true },
+  })
+  use({
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    tag = "v<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!:).
+    run = "make install_jsregexp",
+  })
+  use({
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  })
+  use("mfussenegger/nvim-jdtls")
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  })
+  use("windwp/nvim-ts-autotag")
+  use("windwp/nvim-autopairs")
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.1",
+  })
+  use({
+    "nvim-telescope/telescope-file-browser.nvim",
+    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  })
+  use("nvim-lua/plenary.nvim")
+  use("nvim-tree/nvim-web-devicons")
+  -- LSP
+  use("neovim/nvim-lspconfig")
+  use("jose-elias-alvarez/null-ls.nvim")
+  use("MunifTanjim/prettier.nvim")
+  use("onsails/lspkind-nvim")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/vim-vsnip")
+  use("hrsh7th/vim-vsnip-integ")
+  use("nvim-lua/popup.nvim")
+  use({
+    "startup-nvim/startup.nvim",
+    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require("startup").setup({ theme = "dashboard" })
+    end,
+  })
+  use({ "lewis6991/gitsigns.nvim" })
+  use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
+end)
