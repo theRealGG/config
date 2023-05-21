7e3749b8bfc2f18d644a3b62ae2437056989ce40
Hisham Görner <43104578+theRealGG@users.noreply.github.com>
2023-05-21 15:24:03 +0200

    Add files via upload

---
+++ nvim/lua/tabs.lua
@@ -0,0 +1,10 @@
+local bufferline = require("bufferline")
+
+bufferline.setup({
+	options = {
+		mode = "tabs",
+		show_buffer_close_icons = false,
+		show_close_icon = false,
+		color_icons = true,
+	},
+})
